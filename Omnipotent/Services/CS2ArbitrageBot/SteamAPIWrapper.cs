e60ea875de5011b89db01bf699ba09a25fc3463f
Klives <klivesdev@gmail.com>
2025-06-29 17:06:12 +0100

    fix

--- Omnipotent/Services/CS2ArbitrageBot/SteamAPIWrapper.cs
+++ Omnipotent/Services/CS2ArbitrageBot/SteamAPIWrapper.cs
@@ -69,7 +69,7 @@ namespace Omnipotent.Services.CS2ArbitrageBot
                 listing.PriceInPounds = Convert.ToDouble(listing.PriceInPence) / 100;
                 listing.PriceText = "£" + listing.PriceInPounds.ToString();
                 listing.SellListings = "999";
-                listing.ListingURL = $"https://steamcommunity.com/market/listings/730/{itemHashName}";
+                listing.ListingURL = $"https://steamcommunity.com/market/listings/730/{itemHashName.Replace(" ", "%20")}";
                 try
                 {
                     var assets = jsonObj["assets"]
