e642b952aa972d688a96d138a9c53035715a41eb
Klives <klivesdev@gmail.com>
2025-06-30 16:21:44 +0100

    fix

--- Omnipotent/Services/CS2ArbitrageBot/CS2ArbitrageBot.cs
+++ Omnipotent/Services/CS2ArbitrageBot/CS2ArbitrageBot.cs
@@ -104,6 +104,10 @@ namespace Omnipotent.Services.CS2ArbitrageBot
                             continue;
                         }
                     }
+                    else
+                    {
+                        scanalytics.AllScannedComparisonsInHistory.Add(comparison);
+                    }
                     if ((comparison.PredictedOverallArbitrageGain - 1) * 100 > MinimumPercentReturnToSnipe)
                     {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
