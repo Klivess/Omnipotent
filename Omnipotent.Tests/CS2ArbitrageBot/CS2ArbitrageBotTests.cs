519ad3f974c60757c4010aa1d99517b327d015ae
Klives <klivesdev@gmail.com>
2025-09-17 16:37:43 +0100

    Added Omnipotent.Tests

---
+++ Omnipotent.Tests/CS2ArbitrageBot/CS2ArbitrageBotTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnipotent.Tests.CS2ArbitrageBot
+{
+    public class CS2ArbitrageBotTests
+    {
+        [Fact]
+        public async Task SteamLoginSuccessful()
+        {
+
+        }
+    }
+}
