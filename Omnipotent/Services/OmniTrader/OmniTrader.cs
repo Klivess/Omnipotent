1c014b7bc32b6ef8e8e37acf0217a18ddfba0d8e
Klives <klivesdev@gmail.com>
2026-02-26 15:39:19 +0000

    Add SimpleBacktestLib and prep OmniTrader for backtesting
    Added SimpleBacktestLib NuGet package and new backtesting folder. Removed ETH/USD chart generation code in OmniTrader.cs and imported SimpleBacktestLib, preparing for future backtesting integration.

--- Omnipotent/Services/OmniTrader/OmniTrader.cs
+++ Omnipotent/Services/OmniTrader/OmniTrader.cs
@@ -1,7 +1,9 @@
-﻿using Omnipotent.Data_Handling;
+﻿using Newtonsoft.Json;
+using Omnipotent.Data_Handling;
 using Omnipotent.Service_Manager;
 using Omnipotent.Services.KliveBot_Discord;
 using Omnipotent.Services.OmniTrader.Data;
+using SimpleBacktestLib;
 
 namespace Omnipotent.Services.OmniTrader
 {
@@ -17,9 +19,6 @@ namespace Omnipotent.Services.OmniTrader
         {
             requestKlineData = new RequestKlineData(this);
 
-            var candlesData = await requestKlineData.GetCryptoCandlesDataAsync("ETH", "USD", RequestKlineData.TimeInterval.OneHour, 100);
-            var chart = new CandlestickChartGenerator { Title = "ETH/USD 1H" };
-            chart.SaveChartPng(candlesData, RequestKlineData.TimeInterval.OneHour, @"C:\Charts\eth_usd.png");
         }
     }
 }
