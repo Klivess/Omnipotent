1780468b9a8d83e91ef8f562bbc9a28a303712a6
Klives <kliveskusuo@gmail.com>
2026-02-27 12:13:15 +0000

    removed debug meme compilation

--- Omnipotent/Services/OmniTube/OmniTube.cs
+++ Omnipotent/Services/OmniTube/OmniTube.cs
@@ -19,41 +19,7 @@ namespace Omnipotent.Services.OmniTube
         protected override async void ServiceMain()
         {
             videoFactory = new(this);
-            if (OmniPaths.CheckIfOnServer() == false)
-            {
-                List<InstagramScrapeUtilities.InstagramReel> reels = new();
-                var memeScraper = (MemeScraper.MemeScraper)(await serviceManager.GetServiceByClassType<MemeScraper.MemeScraper>())[0];
-                while (memeScraper.mediaManager == null)
-                {
-                    await Task.Delay(100);
-                }
-                //Copy the reels from memeScraper to avoid threading issues
-                reels.AddRange(memeScraper.mediaManager.allScrapedReels);
-                //Pick 50 random reels
-                var reelsUnder10Seconds = new ConcurrentBag<InstagramScrapeUtilities.InstagramReel>();
-                int error = 0;
-                List<Exception> exceptions = new();
-                await Task.WhenAll(reels.Select(async item =>
-                {
-                    try
-                    {
-                        var analysis = await FFProbe.AnalyseAsync(item.GetInstagramReelVideoFilePath());
-                        if (analysis.Duration.TotalSeconds < 5)
-                        {
-                            reelsUnder10Seconds.Add(item);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        error++;
-                        exceptions.Add(e);
-                    }
-                }));
-                var selection = reelsUnder10Seconds.OrderBy(x => Guid.NewGuid()).Take(50).ToList();
-                string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "MemeCompilation.mp4");
-                //var success = await videoFactory.ProduceMemeCompilation(selection, outputPath);
-                ServiceLog("Meme compilation video created at: " + outputPath);
-            }
+
         }
     }
 }
