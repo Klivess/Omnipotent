79624258eb9bbf46369e2c4eaad64c4458fb9f53
Klivess <klivesdev@gmail.com>
2026-05-08 03:21:09 +0100

    remove omniupdater

--- Omnipotent/Program.cs
+++ Omnipotent/Program.cs
@@ -82,7 +82,7 @@ namespace Omnipotent
                 }
                 omniServiceManager.CreateAndStartNewMonitoredOmniService(new OmniTrader());
                 omniServiceManager.CreateAndStartNewMonitoredOmniService(new KliveMultiTool());
-                omniServiceManager.CreateAndStartNewMonitoredOmniService(new Omnipotent.Services.OmniUpdater.OmniUpdater());
+                //omniServiceManager.CreateAndStartNewMonitoredOmniService(new Omnipotent.Services.OmniUpdater.OmniUpdater());
 
                 testTask();
 
